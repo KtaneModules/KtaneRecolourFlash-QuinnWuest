59f47bf30418d21db0d6cb474e7f7180ed3c6733
JakkOfKlubs <qwuest123@gmail.com>
2022-02-07 15:01:29 -0500

    Animation fixes.

--- Assets/RecolourFlashScript.cs
+++ Assets/RecolourFlashScript.cs
@@ -16,6 +16,7 @@ public class RecolourFlashScript : MonoBehaviour
     public KMSelectable YesButton;
     public KMSelectable NoButton;
     public TextMesh ScreenText;
+    public GameObject[] ButtonObjs;
 
     private int _moduleId;
     private static int _moduleIdCounter = 1;
@@ -85,14 +86,19 @@ public class RecolourFlashScript : MonoBehaviour
     private int _curRow;
     private int _curCol;
     private bool _yesPressed;
+    private int _curFlash;
+    private bool _isFlashingWord;
 
     private Coroutine _flashSequence;
+    private Coroutine[] _pressAnimations = new Coroutine[2];
 
     private void Start()
     {
         _moduleId = _moduleIdCounter++;
         YesButton.OnInteract += YesPress;
         NoButton.OnInteract += NoPress;
+        YesButton.OnInteractEnded += YesRelease;
+        NoButton.OnInteractEnded += NoRelease;
 
         _solution = _chartWords[Rnd.Range(0, _chartWords.Length)];
         GenerateWordSearch();
@@ -110,7 +116,11 @@ public class RecolourFlashScript : MonoBehaviour
 
     private bool YesPress()
     {
+        YesButton.AddInteractionPunch(0.5f);
         Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+        if (_pressAnimations[0] != null)
+            StopCoroutine(_pressAnimations[0]);
+        _pressAnimations[0] = StartCoroutine(PressAnimation(0, true));
         if (!_moduleSolved)
         {
             _yesPressed = true;
@@ -121,13 +131,44 @@ public class RecolourFlashScript : MonoBehaviour
 
     private bool NoPress()
     {
+        NoButton.AddInteractionPunch(0.5f);
         Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+        if (_pressAnimations[1] != null)
+            StopCoroutine(_pressAnimations[1]);
+        _pressAnimations[1] = StartCoroutine(PressAnimation(1, true));
         if (!_moduleSolved)
         {
             _yesPressed = false;
             _pressesDuringTimerTick++;
         }
         return false;
+    }
+
+    private void YesRelease()
+    {
+        if (_pressAnimations[0] != null)
+            StopCoroutine(_pressAnimations[0]);
+        _pressAnimations[0] = StartCoroutine(PressAnimation(0, false));
+    }
+
+    private void NoRelease()
+    {
+        if (_pressAnimations[1] != null)
+            StopCoroutine(_pressAnimations[1]);
+        _pressAnimations[1] = StartCoroutine(PressAnimation(1, false));
+    }
+
+    private IEnumerator PressAnimation(int btn, bool pushIn)
+    {
+        var duration = 0.1f;
+        var elapsed = 0f;
+        var curPos = ButtonObjs[btn].transform.localPosition;
+        while (elapsed < duration)
+        {
+            ButtonObjs[btn].transform.localPosition = new Vector3(curPos.x, Easing.InOutQuad(elapsed, curPos.y, pushIn ? 0.01f : 0.0146f, duration), curPos.z);
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
     }
 
     private int CalcCurPos()
@@ -248,13 +289,15 @@ public class RecolourFlashScript : MonoBehaviour
     {
         while (true)
         {
-            for (int i = 0; i < _flashes.Length; i++)
+            _isFlashingWord = true;
+            for (_curFlash = 0; _curFlash < _flashes.Length; _curFlash++)
             {
-                ScreenText.text = _colourNames[_flashes[i][0]];
-                ScreenText.color = _colours[_flashes[i][1]];
+                ScreenText.text = _colourNames[_flashes[_curFlash][0]];
+                ScreenText.color = _colours[_flashes[_curFlash][1]];
                 yield return new WaitForSeconds(0.5f);
             }
             ScreenText.text = "";
+            _isFlashingWord = false;
             yield return new WaitForSeconds(0.5f);
         }
     }
@@ -262,17 +305,17 @@ public class RecolourFlashScript : MonoBehaviour
     private void GenerateSemaphoreFlash()
     {
         var curLetter = Array.IndexOf("ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray(), _field[_curPos]);
-    tryAgain:
+        tryAgain:
         var hasCyan = false;
         for (int i = 0; i < _flashes.Length; i++)
         {
-        tryAgain2:
+            tryAgain2:
             _flashes[i][0] = Rnd.Range(0, 6);
             if (_semaphores[curLetter].Contains(i))
                 _flashes[i][1] = _flashes[i][0];
             else
             {
-            tryAgain3:
+                tryAgain3:
                 _flashes[i][1] = Rnd.Range(0, 6);
                 if (_flashes[i][1] == _flashes[i][0])
                     goto tryAgain3;
@@ -284,11 +327,16 @@ public class RecolourFlashScript : MonoBehaviour
         }
         if (!hasCyan)
             goto tryAgain;
+        if (_isFlashingWord)
+        {
+            ScreenText.text = _colourNames[_flashes[_curFlash][0]];
+            ScreenText.color = _colours[_flashes[_curFlash][1]];
+        }
     }
 
     private void GenerateWordSearch()
     {
-    tryAgain:
+        tryAgain:
         _field = new char[_w * _h];
         _field[0] = (char)('A' + Rnd.Range(0, 26));
         _field[_w - 1] = (char)('A' + Rnd.Range(0, 26));
@@ -311,7 +359,7 @@ public class RecolourFlashScript : MonoBehaviour
                 }
 
 
-            initialPlaced:;
+        initialPlaced:;
         for (int i = 0; i < _w * _h; i++)
             if (_field[i] == '\0')
                 _field[i] = (char)('A' + Rnd.Range(0, 26));
@@ -446,8 +494,9 @@ public class RecolourFlashScript : MonoBehaviour
         var m = Regex.Match(command, @"^\s*(press\s+)?((?<yes>yes)|no)\s+((?<even>even)|(?<odd>odd)|twice)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
         if (!m.Success)
             yield break;
-
         yield return null;
+        yield return "solve";
+        yield return "strike";
         var btn = m.Groups["yes"].Success ? YesButton : NoButton;
         if (m.Groups["even"].Success)
         {
